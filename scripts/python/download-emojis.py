d96af83160482d2ce5b1b10b81c04eaed043c011
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-11-05 22:26:31 +1300

    Feat: add script that downloads all fluent ui emojis

---
+++ scripts/python/download-emojis.py
@@ -0,0 +1,62 @@
+
+
+import requests
+from bs4 import BeautifulSoup
+import os
+from urllib.parse import urljoin
+
+types = ["Activities", "Animals", "Food", "Hand%20gestures", "Objects", "People%20with%20activities", "People%20with%20professions", "People", "Smilies", "Symbols", "Travel%20and%20places"]
+from time import sleep
+def download_emojis():
+    # Create Animals directory if it doesn't exist
+    for category in types:
+        
+        if not os.path.exists(category.replace("%20", "-")):
+            os.makedirs(category.replace("%20", "-"))
+
+        # GitHub repository URL
+        url = 'https://github.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/tree/master/Emojis/' + category
+        
+        # Get raw content URL base
+        raw_base = 'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/' + category + '/'
+
+        # Fetch the page
+        response = requests.get(url)
+        
+        print(f"Fetching {url}...")
+        print(response)
+        png_files = []
+        for i in range(5):
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            
+            print(soup.find_all('.react-directory-row'))
+
+            # Find all links ending with .png
+            png_files = [link.get('href').split('/')[-1] 
+                        for link in soup.find_all('a') 
+                        if link.get('href', '').endswith('.png')]
+
+            print(f"Found {len(png_files)} PNG files")
+            
+            if(len(png_files) != 0):
+                break
+            sleep(1)
+
+        # Download each PNG
+        for png in png_files:
+            file_url = urljoin(raw_base, png)
+            save_path = os.path.join(category.replace("%20","-"), png.replace("%20", "-"))
+            if(os.path.exists(save_path)):
+                continue
+            print(f"Downloading {png}...")
+            response = requests.get(file_url)
+            
+            if response.status_code == 200:
+                with open(save_path, 'wb') as f:
+                    f.write(response.content)
+            else:
+                print(f"Failed to download {png}")
+
+if __name__ == '__main__':
+    download_emojis()
