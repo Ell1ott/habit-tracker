8f1bc056a4f7136370305da6759cd6080baabb10
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-11-06 22:01:00 +1300

    Feat: update emoji analyse script to write to a ts file, with require and duraction

--- scripts/python/analyse-emojis.py
+++ scripts/python/analyse-emojis.py
@@ -2,41 +2,59 @@ import os
 import json
 from apng import APNG
 
+import re
+
+importFileString = "export const emojis = {"
+
 png_data = {}  # Dictionary to store results
 def analyze_animal_pngs(directory="Animals", output_file="durations.json"):
     """Analyze PNG files and save name/duration pairs to JSON"""
+    global importFileString
     
     
     for file in os.listdir(directory):
         if file.lower().endswith('.png'):
             full_path = os.path.join(directory, file)
+            emojiName = cleanEmojiName(file)
             try:
                 im = APNG.open(full_path)
                 frames = len(im.frames)
                 duration = frames * 42
-                print(f"{file}: {'Animated' if frames > 1 else 'Static'} ({frames} frames) ({duration} ms)")
+                # print(f"{file}: {'Animated' if frames > 1 else 'Static'} ({frames} frames) ({duration} ms)")
                 png_data[file] = duration  # Store in dictionary
+                s = "'" + emojiName + "': {image: require('@/" + full_path.replace("../../", "").replace("\\", "/") + "'), duration: " + str(duration) + "},"
+                print(s)
+                importFileString += s
             except Exception as e:
                 print(f"Error processing {file}: {str(e)}")
-            print(full_path)
+            # print(full_path)
     
     if not png_data:
         print("No PNG files found in directory")
     else:
-        print(f"\nTotal PNG files analyzed: {len(png_data)}")
+        # print(f"\nTotal PNG files analyzed: {len(png_data)}")
         
         # Save to JSON file
         try:
             with open(output_file, 'w') as f:
                 json.dump(png_data, f, indent=2)
-            print(f"Duration data saved to {output_file}")
+            # print(f"Duration data saved to {output_file}")
         except Exception as e:
             print(f"Error saving JSON file: {str(e)}")
+            
+def cleanEmojiName(name):
+    return re.sub(r'%[a-zA-Z]*\d+', '', name.replace(".png", "").replace("-", "").replace("(", "").replace(")", ""))
+    
 
 if __name__ == "__main__":
-    base_dir = "Emojis"
+    base_dir = "../../assets/Emojis"
     for folder in os.listdir(base_dir):
         folder_path = os.path.join(base_dir, folder)
         if os.path.isdir(folder_path):
-            print(f"\nAnalyzing folder: {folder}")
+            # print(f"\nAnalyzing folder: {folder}")
             analyze_animal_pngs(directory=folder_path)
+            
+    importFileString += "}"
+    with open("../../assets/Emoji.ts", 'w') as f:
+        f.write(importFileString)
+    print("Done")
